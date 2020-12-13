e819d35232590478808560001cb6704535fc257a
WHellhaus <wolfgang.hellicksons@gmail.com>
2020-12-12 23:51:45 -0800

    created notebook with options to configure NB model in different ways and confusion matrix for the model

--- NBClassification.py
+++ NBClassification.py
@@ -2,59 +2,37 @@ import pandas as pd
 import numpy as np
 
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 from nltk.probability import FreqDist
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split 
 
-from wordcloud import WordCloud   # pip install wordcloud
-from math import log, sqrt
 
-
-# This function loads spam data. The format of dataset is described in
-# assignment document.
-# Input: file name
-# Output: entire dataset, filterd by hams data, filtered by spams data
-# Note all outputs are in Pandas' dataframe format
-# NOT USED IN THIS CODE (see processGames function instead)
-def LoadData(file_name):
-	games = pd.read_csv(file_name, header=0, encoding='latin-1')
-	data = [games['moves'], games['opening_eco']]
-	ply = games['opening_ply']
-	headers = ['moves', 'opening']
-	data = pd.concat(data, axis=1, keys=headers)
-	# data = data.head(500)
-
+def LoadData():
+	to_exclude = [i for i in range(2, 19800)]# specifies the amount of data to load in (leave list empty to load all data)
+	games = pd.read_csv('games.csv', header=0, encoding='latin-1', skiprows=to_exclude)
 	opening_cats = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+	labels = []
+	for index, row in games.iterrows():
+		labels.append(opening_cats[row['opening_eco'][0]])
+	games = pd.concat([games, pd.DataFrame({'label': labels})], axis=1)
+	headers = list(games.columns.values)
 
-	for index, row in data.iterrows():
-		row['opening'] = opening_cats[row['opening'][0]]
-		moveCount = 0
-		moves = word_tokenize(row['moves'])
-		formattedMoves = []
-		for move in moves:
-			if(moveCount > (ply[index]*2)):
-				break
-			if(moveCount%2==0):
-				# formattedMoves.append(str(int(moveCount/2)+1) + '.' + str(move))
-				formattedMoves.append(str(move))
-			else:
-				formattedMoves[int(moveCount/2)] += ' ' + str(move)
-			moveCount += 1
-		row['moves'] = formattedMoves
-	
-	mcw = []
-	for key in opening_cats:
-		rows = data.loc[data['opening'] == opening_cats[key]]
-		# print(key, rows)
-		moves = []
-		for index, row in rows.iterrows():
-			moves += row['moves']
-		mcw.append(MostCommonWords(moves))
-	return data, mcw
+	X_train, X_test, y_train, y_test = train_test_split(games.to_numpy(), labels, test_size=0.33)
+	X_train = pd.DataFrame(data=X_train, columns=headers)
+	X_test = pd.DataFrame(data=X_test, columns=headers)
 
-def processGames(games, truncate_ply, opening_cats):
+	# dictionary for how to tokenize moves into a list
+	# by ply: split by  each move of white or black
+	# by turn: split by each turn i.e. one white move and one black move
+	# by turn with number: split by turn and add the number of the turn to the beginning of the string (psuedo-dependency)
+	move_tokenizer_options = {'by ply': 0, 'by turn': 1, 'by turn with number': 2}
+
+	games, mcw = processGames(X_train, True, move_tokenizer_options['by turn'], opening_cats)
+	test, mcw_test = processGames(X_test, False, move_tokenizer_options['by turn'], opening_cats)
+	print(test)
+	return games, test, mcw
+
+def processGames(games, truncate_ply, move_tokenizer, opening_cats):
 	data = [games['moves'], games['opening_eco']]
 	ply = games['opening_ply']
 	headers = ['moves', 'opening']
@@ -66,21 +44,34 @@ def processGames(games, truncate_ply, opening_cats):
 		ply[index] = ply[index]+1 if (ply[index] % 2 != 0) else ply[index]
 		moveCount = 0
 		moves = word_tokenize(row['moves'])
-		formattedMoves = []
-		for move in moves:
-			if(truncate_ply): 
-				if (moveCount >= (ply[index])):
-					break
-			elif (moveCount >= maxPly):
-				break
-			if(moveCount%2==0):
-				# uncomment this top line to use moveCount in the moves list i.e. 1. d4 e5, 2. 
-				# formattedMoves.append(str(int(moveCount/2)+1) + '.' + str(move))
-				formattedMoves.append(str(move))
+
+		if (move_tokenizer == 0):
+			if(truncate_ply):
+				row['moves'] = moves[0:ply[index]+1]
 			else:
-				formattedMoves[int(moveCount/2)] += ' ' + str(move)
-			moveCount += 1
-		row['moves'] = formattedMoves
+				row['moves'] = moves[0:maxPly+1]
+		else:
+			formattedMoves = []
+			for move in moves:
+				if(truncate_ply): 
+					if (moveCount >= (ply[index])):
+						break
+				elif (moveCount >= maxPly):
+					break
+
+				if(move_tokenizer == 1):
+					if(moveCount%2==0):
+						formattedMoves.append(str(move))
+					else:
+						formattedMoves[int(moveCount/2)] += ' ' + str(move)
+				if(move_tokenizer == 2):
+					if(moveCount%2==0):
+						formattedMoves.append(str(int(moveCount/2)+1) + '.' + str(move))
+					else:
+						formattedMoves[int(moveCount/2)] += ' ' + str(move)	
+
+				moveCount += 1
+			row['moves'] = formattedMoves
 	
 	mcw = []
 	for key in opening_cats:
@@ -116,7 +107,6 @@ def Classify2(moves, p_category, train_splits, numWords, alpha, categories):
 	for move in moves:
 		for key, value in categories.items():
 			p_cat_given_moves[value] *= Prob_Word_GivenY(move, train_splits[value], numWords, alpha, value)
-	# print(p_cat_given_moves)
 	return p_cat_given_moves.index(max(p_cat_given_moves))
 	
 def Training2(train_data, train_wc, categories, test_data):
@@ -143,31 +133,20 @@ def Training2(train_data, train_wc, categories, test_data):
 	train_splits = [x[1] for x in train_data.groupby('opening')]
 	
 	correct = 0
+	shape = np.zeros(shape=(len(categories), len(categories)))
+	conf_matrix = pd.DataFrame(shape)
 	for i, row in test_data.iterrows():
 		prediction = Classify2(row['moves'], p, train_splits, sum(num_words), alpha, categories)
+		conf_matrix.iat[prediction, row['opening']] += 1
 		correct += 1 if prediction == row['opening'] else 0
-		print(prediction, row['opening'])
+		# print(prediction, row['opening'])
 	print('ACCURACY: ', correct/len(test_data))
+	print(conf_matrix)
 	
 
 def main():
-	to_exclude = [i for i in range(2, 19800)]# specifies the amount of data to load in (leave list empty to load all data)
-	games = pd.read_csv('games.csv', header=0, encoding='latin-1', skiprows=to_exclude)
 	opening_cats = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
-	labels = []
-	for index, row in games.iterrows():
-		labels.append(opening_cats[row['opening_eco'][0]])
-	games = pd.concat([games, pd.DataFrame({'label': labels})], axis=1)
-	headers = list(games.columns.values)
-
-	X_train, X_test, y_train, y_test = train_test_split(games.to_numpy(), labels, test_size=0.33)
-	X_train = pd.DataFrame(data=X_train, columns=headers)
-	X_test = pd.DataFrame(data=X_test, columns=headers)
-
-	games, mcw = processGames(X_train, True, opening_cats)
-	test, mcw_test = processGames(X_test, False, opening_cats)
-	print(games.head(5))
-	print(test.head(5))
+	games, test,  mcw = LoadData()
 	Training2(games, mcw, opening_cats, test)
 	
 main()
